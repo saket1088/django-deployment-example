5d486a9332c0e3428d537b6e4836c1de0047490e
span104 <saketj@spanidea.com>
2019-10-17 12:35:15 +0530

    first

---
+++ learning_users/MyVirtualEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/span104/Desktop/udemy/django-practice/level_five_userLogin/learning_users/MyVirtualEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
